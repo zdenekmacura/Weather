3c0588a09ac2064bb4e6a04a834777befdc89e2a
zdenekmacura <zdenek.macura@email.cz>
2015-03-24 01:11:49 +0100

    first commit

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,8 @@
 
 $(document).ready(function(){
 	$("#desc").show(1000);
-	 navigator.geolocation.getCurrentPosition(onSuccess, onError);
-	 function onSuccess(position) {
+	 //navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	 /*function onSuccess(position) {
         var element = document.getElementById('geolocation');
         element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
                             'Longitude: '          + position.coords.longitude             + '<br />' +
@@ -13,7 +13,7 @@ $(document).ready(function(){
                             'Speed: '              + position.coords.speed                 + '<br />' +
                             'Timestamp: '          + position.timestamp                    + '<br />';
 
-    }
+    }*/
 	$.getJSON('http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139', function(jd) {
 			 $('#stage').html('<p> Current weather is:<b>' + jd.weather[0].description + ' </b></p>');
              $('#stage2').html('<p> Image: <img src="http://openweathermap.org/img/w/' + jd.weather[0].icon + '.png"></p>');
